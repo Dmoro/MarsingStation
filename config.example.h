da62b607968b80c9f8277e542d42c84d44667f09
Dmoro <moro.daniele@gmail.com>
2016-06-02 12:50:32 -0600

    Update config.example.h

--- config.example.h
+++ config.example.h
@@ -3,8 +3,8 @@
 #define APIKEY String("0123456789ABCDEF")
 #define CUSTOM_DATA_SERVER_IP 0
 #define CUSTOM_DATA_SERVER_PASSWORD String("1234")
-#define SENSORS {new DHT22Sensor(2)}
-#define SENSORS_LENGTH 1
-#define OUTPUTS {new SerialOutput()}
-#define OUTPUTS_LENGTH 1
-#define DELAY 5000
+#define SENSORS {new DHT22Sensor(5), new ThermistorSensor(A2)}
+#define SENSORS_LENGTH 2
+#define OUTPUTS {new SerialOutput(), SDCardOutput()}
+#define OUTPUTS_LENGTH 2
+#define DELAY 10000
